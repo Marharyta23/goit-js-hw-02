f246988caf23104b43367c2d2e5a4ea6c5ef0106
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2023-11-20 00:09:25 +0200

    fixed

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,9 @@
 function formatMessage(message, maxLength) {
   let messageLength = message.length;
   if (messageLength <= maxLength) {
-    console.log(message);
+    return message;
   } else {
-    console.log(message.slice(0, maxLength) + "...");
+    const transformedMessage = message.slice(0, maxLength) + "...";
+    return transformedMessage;
   }
-  return;
 }
-
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
